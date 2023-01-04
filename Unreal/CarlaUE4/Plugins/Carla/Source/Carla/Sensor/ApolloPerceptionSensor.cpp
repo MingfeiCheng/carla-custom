1743390ca1f160cbc01c0aca856208cf28dcd9b3
MingfeiCheng <snowbirds.mf@gmail.com>
2023-01-04 16:19:06 +0800

    add apollo chassis sensor

---
+++ Unreal/CarlaUE4/Plugins/Carla/Source/Carla/Sensor/ApolloPerceptionSensor.cpp
@@ -0,0 +1,70 @@
+// Copyright (c) 2019 Computer Vision Center (CVC) at the Universitat Autonoma
+// de Barcelona (UAB).
+//
+// This work is licensed under the terms of the MIT license.
+// For a copy, see <https://opensource.org/licenses/MIT>.
+
+#include <exception>
+#include <fstream>
+
+#include "Carla.h"
+#include "Carla/Sensor/ApolloChassisSensor.h"
+#include "Carla/Game/CarlaEpisode.h"
+#include "Carla/Game/CarlaStatics.h"
+#include "Carla/MapGen/LargeMapManager.h"
+#include "Carla/Actor/ActorBlueprintFunctionLibrary.h"
+#include "Carla/Sensor/WorldObserver.h"
+
+#include <compiler/disable-ue4-macros.h>
+#include "carla/client/Vehicle.h"
+#include "carla/rpc/VehicleControl.h"
+#include <compiler/enable-ue4-macros.h>
+
+AApolloChassisSensor::AApolloChassisSensor(const FObjectInitializer &ObjectInitializer)
+  : Super(ObjectInitializer)
+{
+  PrimaryActorTick.bCanEverTick = true;
+}
+
+FActorDefinition AApolloChassisSensor::GetSensorDefinition()
+{
+  return UActorBlueprintFunctionLibrary::MakeGenericSensorDefinition(TEXT("apollo"), TEXT("chassis"));
+}
+
+void AApolloChassisSensor::SetOwner(AActor *Owner)
+{
+  Super::SetOwner(Owner);
+}
+
+void AApolloChassisSensor::PostPhysTick(UWorld *World, ELevelTick TickType, float DeltaSeconds)
+{
+  TRACE_CPUPROFILER_EVENT_SCOPE(AApolloChassisSensor::PostPhysTick);
+
+  if (GetParent() == nullptr) {
+    throw_exception(std::runtime_error(GetDisplayId() + ": not attached to actor"));
+    return;
+  }
+
+  const auto carla_ego_vehicle = boost::dynamic_pointer_cast<carla::client::Vehicle>(GetParent());
+  if (carla_ego_vehicle == nullptr) {
+    _logger->error("Apollo Chassis Sensor only support vehicles as ego.");
+  }
+
+  carla::rpc::VehicleControl CurrentVehicleControl = carla_ego_vehicle.GetControl()
+
+  {
+    TRACE_CPUPROFILER_EVENT_SCOPE_STR("AApolloChassisSensor Stream Send");
+    auto Stream = GetDataStream(*this);
+    Stream.Send(
+      *this, 
+      CurrentVehicleControl);
+  }
+}
+
+void AApolloChassisSensor::BeginPlay()
+{
+  Super::BeginPlay();
+
+  const UCarlaEpisode* episode = UCarlaStatics::GetCurrentEpisode(GetWorld());
+  CurrentGeoReference = episode->GetGeoReference();
+}
