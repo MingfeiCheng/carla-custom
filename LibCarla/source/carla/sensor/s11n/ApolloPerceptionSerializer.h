3c5669f2b2cf1b27f0a44b85f554cf20ac8ff256
MingfeiCheng <snowbirds.mf@gmail.com>
2023-01-06 20:57:34 +0800

    update sensors

--- LibCarla/source/carla/sensor/s11n/ApolloPerceptionSerializer.h
+++ LibCarla/source/carla/sensor/s11n/ApolloPerceptionSerializer.h
@@ -11,6 +11,13 @@
 #include "carla/rpc/Actor.h"
 #include "carla/rpc/PerceptionUnit.h"
 #include "carla/sensor/RawData.h"
+#include "carla/client/detail/ActorVariant.h"
+#include "carla/client/Actor.h"
+#include "carla/geom/BoundingBox.h"
+#include "carla/geom/Vector3D.h"
+#include "carla/geom/Location.h"
+#include "carla/geom/Rotation.h"
+#include "carla/rpc/ActorId.h"
 
 #include <cstdint>
 #include <cstring>
@@ -28,12 +35,31 @@ namespace s11n {
     template <typename SensorT, typename EpisodeT, typename PerceptionUnitListT>
     static Buffer Serialize(
         const SensorT &,
-        const PerceptionUnitListT &detected_actors) {
+        const EpisodeT &episode,
+        const ActorListT &detected_actors) {
       const uint32_t size_in_bytes = sizeof(rpc::PerceptionUnit) * detected_actors.Num();
       Buffer buffer{size_in_bytes};
       unsigned char *it = buffer.data();
       for (auto *actor : detected_actors) {
-        std::memcpy(it, &actor, sizeof(rpc::PerceptionUnit));
+        const carla::rpc::Actor actor_info = episode.SerializeActor(actor);
+        carla::rpc::ActorId actor_id = actor_info.id;
+        carla::geom::BoundingBox actor_bbox = actor_info.bounding_box;
+        std::string actor_type = actor_info.description.id; //TODO:Check
+        const carla::client::detail::ActorVariant actor_variant = carla::client::detail::ActorVariant(actor_info);
+        const carla::SharedPtr<carla::client::Actor> actor_ptr = actor_variant.Get(episode);
+
+        const carla::geom::Location actor_location = actor_ptr.GetLocation();
+        const carla::geom::Rotation actor_rotation = actor_ptr.GetRotation();
+        const carla::geom::Vector3D actor_velocity = actor_ptr.GetVelocity();
+        const carla::geom::Vector3D actor_acceleration = actor_ptr.GetAcceleration();
+
+        const carla::geom::Location apollo_actor_location = carla::geom::Location(actor_location.x, -actor_location.y, actor_location.z);
+        const carla::geom::Vector3D apollo_actor_velocity = carla::geom::Vector3D(actor_velocity.x, -actor_velocity.y, actor_velocity.z);
+        const carla::geom::Vector3D apollo_actor_acceleration = carla::geom::Vector3D(actor_acceleration.x, -actor_acceleration.y, actor_acceleration.z);
+
+        const carla::rpc::PerceptionUnit actor_perception_unit = carla::rpc::PerceptionUnit(actor_id, actor_bbox, actor_type, actor_location, actor_rotation, actor_velocity, actor_acceleration);
+
+        std::memcpy(it, &actor_perception_unit, sizeof(rpc::PerceptionUnit));
         it += sizeof(rpc::PerceptionUnit);
       }
       return buffer;
