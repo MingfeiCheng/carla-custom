65436e79e06f45af9c083270af1b33996a9fcc55
MingfeiCheng <snowbirds.mf@gmail.com>
2023-01-07 14:46:11 +0800

    update perception sensor

--- LibCarla/source/carla/sensor/data/ApolloPerceptionMeasurement.h
+++ LibCarla/source/carla/sensor/data/ApolloPerceptionMeasurement.h
@@ -31,8 +31,8 @@ namespace data {
       // actors_rpc = Array<rpc::Actor>(0u, std::move(data));
       // actors = std::vector<SharedPtr<client::Actor>>();
       size_t pos;
-      for (pos = 0; pos < actors_rpc.size(); pos++) {
-        const client::detail::ActorVariant actor_variant = client::detail::ActorVariant(Serializer.DeserializeRawData(actors_rpc.at(pos)));
+      for (pos = 0; pos < _actors_rpc.size(); pos++) {
+        const client::detail::ActorVariant actor_variant = client::detail::ActorVariant(Serializer.DeserializeRawData(_actors_rpc.at(pos)));
         SharedPtr<client::Actor> actor_client = actor_variant.Get(GetEpisode());
         _actors.push_back(actor_client);
       }
