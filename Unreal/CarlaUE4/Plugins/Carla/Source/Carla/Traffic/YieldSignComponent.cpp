4bb6852c32065f097045d1b1b8d5222ea83cc4fc
MingfeiCheng <snowbirds.mf@gmail.com>
2023-01-08 18:30:06 +0800

    update perception sensor

--- Unreal/CarlaUE4/Plugins/Carla/Source/Carla/Traffic/YieldSignComponent.cpp
+++ Unreal/CarlaUE4/Plugins/Carla/Source/Carla/Traffic/YieldSignComponent.cpp
@@ -10,6 +10,7 @@
 
 #include <compiler/disable-ue4-macros.h>
 #include <carla/road/element/RoadInfoSpeed.h>
+#include <carla/road/element/RoadInfoSignal.h>
 #include <compiler/enable-ue4-macros.h>
 
 void UYieldSignComponent::InitializeSign(const carla::road::Map &Map)
