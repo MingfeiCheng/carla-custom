b615d5388bcef920c93be6e48c02686208a3651b
MingfeiCheng <snowbirds.mf@gmail.com>
2023-01-13 01:05:53 +0800

    update carla perception

--- Unreal/CarlaUE4/Plugins/Carla/Source/Carla/Sensor/ApolloPerceptionSensor.cpp
+++ Unreal/CarlaUE4/Plugins/Carla/Source/Carla/Sensor/ApolloPerceptionSensor.cpp
@@ -112,179 +112,106 @@ void AApolloPerceptionSensor::PostPhysTick(UWorld *World, ELevelTick TickType, f
   // Super::PostPhysTick(DeltaSeconds);
   TRACE_CPUPROFILER_EVENT_SCOPE(AApolloPerceptionSensor::PostPhysTick);
 
-  TSet<AActor *> DetectedActors;
+  // TSet<AActor *> DetectedActors;
   // Box->GetOverlappingActors(DetectedActors, ACarlaWheeledVehicle::StaticClass());
-  Box->GetOverlappingActors(DetectedActors, FCarlaActor::StaticClass());
-  DetectedActors.Remove(GetOwner());
+  // Box->GetOverlappingActors(DetectedActors, FCarlaActor::StaticClass());
+  // DetectedActors.Remove(GetOwner());
 
-  if (DetectedActors.Num() > 0){
-    {      
-      // const FActorRegistry &registry = episode.GetActorRegistry();
-      // const UCarlaEpisode* Episode = UCarlaStatics::GetCurrentEpisode(GetWorld());
-      // const auto &Episode = GetEpisode();
-      ObstacleArray ApolloObstacles;
-      constexpr float TO_METERS = 1e-2;
+  const FActorRegistry &Registry = GetEpisode().GetActorRegistry();
+  int obstacle_num = 0;
 
-      for(auto& It : DetectedActors){
-        // const FCarlaActor* ActorView = It.Value.Get();
-        const FCarlaActor* ActorView = GetEpisode().FindCarlaActor(It);
-        const FActorInfo* ActorInfo = ActorView->GetActorInfo();
+  // get owner
+  const FCarlaActor* OwnerActor = GetEpisode().FindCarlaActor(GetOwner());
+  const carla::rpc::ActorId OwnerActorId = OwnerActor->GetActorId();
+  ObstacleArray ApolloObstacles;
+  constexpr float TO_METERS = 1e-2;
 
-        // const carla::rpc::ActorId ApolloActorId = ActorView->GetActorId();
-        // FBoundingBox ActorBBox = UBoundingBoxCalculator::GetActorBoundingBox(It);
-        // const carla::geom::Location ActorBBoxLocation = carla::geom::Location(ActorBBox.Origin.X * TO_METERS, ActorBBox.Origin.Y * TO_METERS, ActorBBox.Origin.Z * TO_METERS);
-        // const carla::geom::Vector3D ActorBBoxExtent = carla::geom::Vector3D(ActorBBox.Extent.X * TO_METERS, ActorBBox.Extent.Y * TO_METERS, ActorBBox.Extent.Z * TO_METERS);
-        // const carla::geom::Rotation ActorBBoxRotation = carla::geom::Rotation(ActorBBox.Rotation.Pitch, ActorBBox.Rotation.Yaw, ActorBBox.Rotation.Roll);
-        // const carla::geom::BoundingBox ApolloActorBBox = carla::geom::BoundingBox(ActorBBoxLocation, ActorBBoxExtent, ActorBBoxRotation);
-        // FCarlaActor::ActorType ActorType = ActorView->GetActorType();
-        // std::string ApolloActorType = "unknown";
-        
-        // if (FCarlaActor::ActorType::Vehicle == ActorType){
-        //   ApolloActorType = "dynamic.vehicle";
-        // }else if (FCarlaActor::ActorType::Walker == ActorType){
-        //   ApolloActorType = "dynamic.walker";
-        // }else{
-        //   ApolloActorType = "static";
-        // }
-
-        // if (ApolloActorType.compare("static") == 0){
-        //   FTransform ActorTransform;
-        //   FVector Velocity(0.0f);
-        //   // FVector Acceleration(0.0f);
-        //   ActorTransform = ActorView->GetActorGlobalTransform();
-        //   const FVector ActorLocation = ActorTransform.GetLocation();
-        //   const FRotator ActorRotation = ActorTransform.GetRotation().Rotator();    
-
-        //   carla::geom::Vector3D ApolloActorAcceleration = carla::geom::Vector3D(0.0f, 0.0f, 0.0f);
-        //   carla::geom::Vector3D ApolloActorVelocity = carla::geom::Vector3D(0.0f, 0.0f, 0.0f);
-        //   const carla::geom::Location ApolloActorLocation = carla::geom::Location(ActorLocation.X * TO_METERS, -ActorLocation.Y * TO_METERS, ActorLocation.Z * TO_METERS);
-        //   const carla::geom::Rotation ApolloActorRotation = carla::geom::Rotation(ActorRotation.Pitch, ActorRotation.Yaw, ActorRotation.Roll);
-
-        //   ApolloObstacles.push_back(carla::sensor::data::ApolloObstacle(ApolloActorId, 
-        //                                                                 ApolloActorType,
-        //                                                                 ApolloActorBBox,
-        //                                                                 ApolloActorRotation,
-        //                                                                 ApolloActorLocation,
-        //                                                                 ApolloActorVelocity,
-        //                                                                 ApolloActorAcceleration));
-        // }else{
-        //   FTransform ActorTransform;
-        //   FVector Velocity(0.0f);
-        //   // FVector Acceleration(0.0f);
-
-        //   check(ActorView);
-
-        //   if(ActorView->IsDormant())
-        //   {
-        //     const FActorData* ActorData = ActorView->GetActorData();
-        //     Velocity = TO_METERS * ActorData->Velocity;
-        //     // AngularVelocity = carla::geom::Vector3D
-        //     //                   {ActorData->AngularVelocity.X,
-        //     //                   ActorData->AngularVelocity.Y,
-        //     //                   ActorData->AngularVelocity.Z};
-        //     // Acceleration = FWorldObserver_GetAcceleration(*View, Velocity, DeltaSeconds);
-        //     // State = FWorldObserver_GetDormantActorState(*View, Registry);
-        //   }
-        //   else
-        //   {
-        //     Velocity = TO_METERS * ActorView->GetActor()->GetVelocity();
-        //     // AngularVelocity = FWorldObserver_GetAngularVelocity(*View->GetActor());
-        //     // Acceleration = FWorldObserver_GetAcceleration(*View, Velocity, DeltaSeconds);
-        //     // State = FWorldObserver_GetActorState(*View, Registry);
-        //   }
-
-        //   FVector &PreviousVelocity = ActorView->GetActorInfo()->Velocity;
-        //   const FVector Acceleration = (Velocity - PreviousVelocity) / DeltaSeconds;
-        //   PreviousVelocity = Velocity;
-        //   ActorTransform = ActorView->GetActorGlobalTransform();
-        //   const FVector ActorLocation = ActorTransform.GetLocation();
-        //   const FRotator ActorRotation = ActorTransform.GetRotation().Rotator();    
-
-        //   carla::geom::Vector3D ApolloActorAcceleration = carla::geom::Vector3D(Acceleration.X, -Acceleration.Y, Acceleration.Z);
-        //   carla::geom::Vector3D ApolloActorVelocity = carla::geom::Vector3D(Velocity.X, -Velocity.Y, Velocity.Z);
-        //   const carla::geom::Location ApolloActorLocation = carla::geom::Location(ActorLocation.X * TO_METERS, -ActorLocation.Y * TO_METERS, ActorLocation.Z * TO_METERS);
-        //   const carla::geom::Rotation ApolloActorRotation = carla::geom::Rotation(ActorRotation.Pitch, ActorRotation.Yaw, ActorRotation.Roll);
-
-        //   ApolloObstacles.push_back(carla::sensor::data::ApolloObstacle(ApolloActorId, 
-        //                                                                 ApolloActorType,
-        //                                                                 ApolloActorBBox,
-        //                                                                 ApolloActorRotation,
-        //                                                                 ApolloActorLocation,
-        //                                                                 ApolloActorVelocity,
-        //                                                                 ApolloActorAcceleration));
-        //   }
-        // }
-
-        const carla::rpc::ActorId ApolloActorId = ActorView->GetActorId();
-        FBoundingBox ActorBBox = UBoundingBoxCalculator::GetActorBoundingBox(It);
-        const carla::geom::Location ActorBBoxLocation = carla::geom::Location(0.0f, 0.0f, 0.0f);
-        const carla::geom::Vector3D ActorBBoxExtent = carla::geom::Vector3D(0.0f, 0.0f, 0.0f);
-        const carla::geom::Rotation ActorBBoxRotation = carla::geom::Rotation(0.0f, 0.0f, 0.0f);
-        const carla::geom::BoundingBox ApolloActorBBox = carla::geom::BoundingBox(ActorBBoxLocation, ActorBBoxExtent, ActorBBoxRotation);
-        // FCarlaActor::ActorType ActorType = ActorView->GetActorType();
-        std::string ApolloActorType = "unknown";
-        
-        // if (FCarlaActor::ActorType::Vehicle == ActorType){
-        //   ApolloActorType = "dynamic.vehicle";
-        // }else if (FCarlaActor::ActorType::Walker == ActorType){
-        //   ApolloActorType = "dynamic.walker";
-        // }else{
-        //   ApolloActorType = "static";
-        // }
-
-        // FTransform ActorTransform;
-        // FVector Velocity(0.0f);
-        // // FVector Acceleration(0.0f);
-
-        // check(ActorView);
-
-        // if(ActorView->IsDormant())
-        // {
-        //   const FActorData* ActorData = ActorView->GetActorData();
-        //   Velocity = TO_METERS * ActorData->Velocity;
-        //   // AngularVelocity = carla::geom::Vector3D
-        //   //                   {ActorData->AngularVelocity.X,
-        //   //                   ActorData->AngularVelocity.Y,
-        //   //                   ActorData->AngularVelocity.Z};
-        //   // Acceleration = FWorldObserver_GetAcceleration(*View, Velocity, DeltaSeconds);
-        //   // State = FWorldObserver_GetDormantActorState(*View, Registry);
-        // }
-        // else
-        // {
-        //   Velocity = TO_METERS * ActorView->GetActor()->GetVelocity();
-        //   // AngularVelocity = FWorldObserver_GetAngularVelocity(*View->GetActor());
-        //   // Acceleration = FWorldObserver_GetAcceleration(*View, Velocity, DeltaSeconds);
-        //   // State = FWorldObserver_GetActorState(*View, Registry);
-        // }
-
-        // FVector &PreviousVelocity = ActorView->GetActorInfo()->Velocity;
-        // const FVector Acceleration = (Velocity - PreviousVelocity) / DeltaSeconds;
-        // PreviousVelocity = Velocity;
-        // ActorTransform = ActorView->GetActorGlobalTransform();
-        // const FVector ActorLocation = ActorTransform.GetLocation();
-        // const FRotator ActorRotation = ActorTransform.GetRotation().Rotator();    
-
-        carla::geom::Vector3D ApolloActorAcceleration = carla::geom::Vector3D(0.0f, 0.0f, 0.0f);
-        carla::geom::Vector3D ApolloActorVelocity = carla::geom::Vector3D(0.0f, 0.0f, 0.0f);
-        const carla::geom::Location ApolloActorLocation = carla::geom::Location(0.0f, 0.0f, 0.0f);
-        const carla::geom::Rotation ApolloActorRotation = carla::geom::Rotation(0.0f, 0.0f, 0.0f);
-
-        ApolloObstacles.push_back(carla::sensor::data::ApolloObstacle(ApolloActorId, 
-                                                                      ApolloActorType,
-                                                                      ApolloActorBBox,
-                                                                      ApolloActorRotation,
-                                                                      ApolloActorLocation,
-                                                                      ApolloActorVelocity,
-                                                                      ApolloActorAcceleration));
-
-      TRACE_CPUPROFILER_EVENT_SCOPE_STR("AApolloPerceptionSensor Stream Send");
-      auto Stream = GetDataStream(*this);
-      // auto Buffer = Stream.PopBufferFromPool();
-      Stream.Send(*this, ApolloObstacles);
-
+  for (auto& It : Registry){
+    const FCarlaActor* ActorView = It.Value.Get();
+    const FActorInfo* ActorInfo = ActorView->GetActorInfo();
+    const carla::rpc::ActorId ApolloActorId = ActorView->GetActorId();
+    if(OwnerActorId==ApolloActorId){
+      continue;
     }
+
+    FCarlaActor::ActorType ActorType = ActorView->GetActorType();
+    std::string ApolloActorType = "unknown";
+    if (FCarlaActor::ActorType::Vehicle == ActorType){
+      ApolloActorType = "dynamic.vehicle";
+    }else if (FCarlaActor::ActorType::Walker == ActorType){
+      ApolloActorType = "dynamic.walker";
+    }else if (FCarlaActor::ActorType::TrafficLight == ActorType){
+      ApolloActorType = "static.traffic_light";
+    }else if (FCarlaActor::ActorType::TrafficSign == ActorType){
+      ApolloActorType = "static.traffic_sign";
+    }else if (FCarlaActor::ActorType::Sensor == ActorType){
+      continue;
+    }else if (FCarlaActor::ActorType::INVALID == ActorType){
+      continue;
+    }else{
+      ApolloActorType = "static.other";
+    }
+
+    FBoundingBox ActorBBox = UBoundingBoxCalculator::GetActorBoundingBox(ActorView->GetActor());
+    const carla::geom::Location ActorBBoxLocation = carla::geom::Location(ActorBBox.Origin.X * TO_METERS, ActorBBox.Origin.Y * TO_METERS, ActorBBox.Origin.Z * TO_METERS);
+    const carla::geom::Vector3D ActorBBoxExtent = carla::geom::Vector3D(ActorBBox.Extent.X * TO_METERS, ActorBBox.Extent.Y * TO_METERS, ActorBBox.Extent.Z * TO_METERS);
+    const carla::geom::Rotation ActorBBoxRotation = carla::geom::Rotation(ActorBBox.Rotation.Pitch, ActorBBox.Rotation.Yaw, ActorBBox.Rotation.Roll);
+    const carla::geom::BoundingBox ApolloActorBBox = carla::geom::BoundingBox(ActorBBoxLocation, ActorBBoxExtent, ActorBBoxRotation);
+
+    FTransform ActorTransform;
+    FVector Velocity(0.0f);
+    // FVector Acceleration(0.0f);
+
+    check(ActorView);
+
+    if(ActorView->IsDormant())
+    {
+      const FActorData* ActorData = ActorView->GetActorData();
+      Velocity = TO_METERS * ActorData->Velocity;
+      // AngularVelocity = carla::geom::Vector3D
+      //                   {ActorData->AngularVelocity.X,
+      //                   ActorData->AngularVelocity.Y,
+      //                   ActorData->AngularVelocity.Z};
+      // Acceleration = FWorldObserver_GetAcceleration(*View, Velocity, DeltaSeconds);
+      // State = FWorldObserver_GetDormantActorState(*View, Registry);
+    }
+    else
+    {
+      Velocity = TO_METERS * ActorView->GetActor()->GetVelocity();
+      // AngularVelocity = FWorldObserver_GetAngularVelocity(*View->GetActor());
+      // Acceleration = FWorldObserver_GetAcceleration(*View, Velocity, DeltaSeconds);
+      // State = FWorldObserver_GetActorState(*View, Registry);
+    }
+
+    FVector &PreviousVelocity = ActorView->GetActorInfo()->Velocity;
+    const FVector Acceleration = (Velocity - PreviousVelocity) / DeltaSeconds;
+    PreviousVelocity = Velocity;
+    ActorTransform = ActorView->GetActorGlobalTransform();
+    const FVector ActorLocation = ActorTransform.GetLocation();
+    const FRotator ActorRotation = ActorTransform.GetRotation().Rotator();    
+
+    carla::geom::Vector3D ApolloActorAcceleration = carla::geom::Vector3D(Acceleration.X, -Acceleration.Y, Acceleration.Z);
+    carla::geom::Vector3D ApolloActorVelocity = carla::geom::Vector3D(Velocity.X, -Velocity.Y, Velocity.Z);
+    const carla::geom::Location ApolloActorLocation = carla::geom::Location(ActorLocation.X * TO_METERS, -ActorLocation.Y * TO_METERS, ActorLocation.Z * TO_METERS);
+    const carla::geom::Rotation ApolloActorRotation = carla::geom::Rotation(ActorRotation.Pitch, ActorRotation.Yaw, ActorRotation.Roll);
+
+    ApolloObstacles.push_back(carla::sensor::data::ApolloObstacle(ApolloActorId, 
+                                                                  ApolloActorType,
+                                                                  ApolloActorBBox,
+                                                                  ApolloActorRotation,
+                                                                  ApolloActorLocation,
+                                                                  ApolloActorVelocity,
+                                                                  ApolloActorAcceleration));
+    obstacle_num = obstacle_num + 1;                                                     
   }
 
+  if (obstacle_num > 0){
+    TRACE_CPUPROFILER_EVENT_SCOPE_STR("AApolloPerceptionSensor Stream Send");
+    auto Stream = GetDataStream(*this);
+    // auto Buffer = Stream.PopBufferFromPool();
+    Stream.Send(*this, ApolloObstacles);
+  }
+  
 }
 
   void AApolloPerceptionSensor::BeginPlay()
